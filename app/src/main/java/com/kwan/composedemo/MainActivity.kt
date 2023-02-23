40b377d4ceefef9554028bb3bf7dad1beddc904d
zgz <1767919808@qq.com>
2023-02-23 18:59:12 +0800

    compose入门: 1.引入compose 2.初步使用Text

--- app/src/main/java/com/kwan/composedemo/MainActivity.kt
+++ app/src/main/java/com/kwan/composedemo/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.kwan.composedemo
 
+import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            HelloWorld()
+        }
     }
+
+    @Composable
+    private fun HelloWorld() {
+        Text(
+//            text = "Hello World!",
+            stringResource(R.string.hello_world, 2),
+            modifier = Modifier.width(400.dp),
+            color = Color.Blue,
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.W400,
+            fontFamily = FontFamily.SansSerif,
+            textAlign = TextAlign.Center
+        )
+    }
+
 }
