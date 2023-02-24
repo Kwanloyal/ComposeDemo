ae28682c9e194c068fb2a85bc046090334fb74b0
zgz <1767919808@qq.com>
2023-02-24 12:10:48 +0800

    compose入门: 1.clickable,fillMaxWidth,Column/horizontalAlignment/verticalArrangement/verticalScroll

--- app/src/main/java/com/kwan/composedemo/MainActivity.kt
+++ app/src/main/java/com/kwan/composedemo/MainActivity.kt
@@ -3,10 +3,17 @@ package com.kwan.composedemo
 import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -28,17 +35,51 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     private fun HelloWorld() {
-        Text(
-//            text = "Hello World!",
-            stringResource(R.string.hello_world, 2),
-            modifier = Modifier.width(400.dp),
-            color = Color.Blue,
-            fontSize = 30.sp,
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.W400,
-            fontFamily = FontFamily.SansSerif,
-            textAlign = TextAlign.Center
-        )
+        Column(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+//               text = "Hello World!",
+                stringResource(R.string.hello_world, 2),
+                modifier = Modifier
+                    .background(Color.Black)
+                    .width(100.dp)
+                    .padding(20.dp)
+                    .clickable {
+                        Toast
+                            .makeText(this@MainActivity, "Hello World!", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                color = Color.White,
+                fontSize = 30.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.W400,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Center
+            )
+            Divider(color = Color.Black)
+            Text(text = "Hello World3", color = Color.Blue)
+            ListMessage(messageList = listOf("text1", "text2", "text3", "text4", "text5", "text6", "text7", "text8", "text9", "text10"))
+        }
+    }
+
+    @Composable
+    private fun ListMessage(messageList: List<String>) {
+        Column(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.LightGray)
+                .verticalScroll(rememberScrollState())
+        ) {
+            messageList.forEach { message ->
+                Text(text = message)
+            }
+        }
     }
 
 }
